c9e333d8def3381aa796b61822d6081c764e05af
feynbin <feynbin@qq.com>
2025-02-03 22:52:48 +0800

    仓库初始化

---
+++ src/main/java/org/example/actionsmaven/ActionsMavenApplication.java
@@ -0,0 +1,13 @@
+package org.example.actionsmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionsMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActionsMavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/actionsmaven/ActionsMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.actionsmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionsMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
